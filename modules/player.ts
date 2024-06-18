1f35c11e80cb85d5d2258e78faaa076bc9e1ed3e
BikramNischal <bikram.bh10@gmail.com>
2024-06-18 05:38:54 +0000

    adds jump mechanics

--- modules/player.ts
+++ modules/player.ts
@@ -9,14 +9,28 @@ export default class Player {
 	h: number;
 	img: HTMLImageElement;
 	loaded: boolean;
+	horizontalDelay: number;
+	gravity: number;
+	jumpHeight: number;
+	isJumping: boolean;
+	jumpCount: number;
+	xDirection: number;
 
 	constructor(posx: number, posy: number, imgsrc: string) {
 		this.x = posx;
 		this.y = posy;
-		this.dx = 2;
-		this.dy = 2;
 		this.w = 32;
 		this.h = 32;
+		
+		// movement agents and variables 
+		this.xDirection = -1;
+		this.dx = 5;
+		this.dy = -12;
+		this.horizontalDelay = 0.1;
+		this.gravity = 0.5;
+		this.jumpHeight = -10;
+		this.isJumping = false;
+		this.jumpCount = 2;
 
 		this.loaded = false;
 		this.img = new Image();
@@ -25,17 +39,35 @@ export default class Player {
 		this.img.onload = () => {
 			this.loaded = true;
 			this.draw();
-			// Draw the outline
-			ctx.strokeStyle = "red"; // Set the outline color
-			ctx.lineWidth = 2; // Set the outline width
-			ctx.strokeRect(this.x, this.y, this.w, this.h);
 		};
 	}
 
 	draw() {
 		if (this.loaded)
 			ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-
-        
 	}
+
+	resetJump(){
+		//change direction
+		this.xDirection *= -1;	
+		this.dx = 5*this.xDirection;
+		this.dy = -12;
+	}
+
+	jump() {
+		this.dy += this.gravity;
+		this.dx =
+			this.dx > 0
+				? this.dx - this.horizontalDelay
+				: this.dx + this.horizontalDelay;
+		this.x += this.dx;
+		this.y += this.dy;
+
+		// Limit falling speed
+		if (this.dy > 10) {
+			this.dy = 10;
+		}
+	}
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ctx } from "../modules/canvas.ts";
 import { CANVAS_HEIGHT, CANVAS_WIDTH, WALL_HEIGHT_SMALL } from "../utils/constants.ts";
-
 import {
 	generateWalls,
 	generateBlades,
@@ -8,31 +7,39 @@ import {
 	updateBlades,
 } from "./generateGameObjects.ts";
 
-import Blade from "../modules/blade.ts";
-import random from "../utils/random.ts";
+// import Blade from "../modules/blade.ts";
+// import random from "../utils/random.ts";
 import Player from "../modules/player.ts";
 import Wall from "../modules/wall.ts";
-// import { calcx } from "../modules/wall.ts";
-
 
 
 const gameWindow = document.querySelector(".game-window") as HTMLDivElement;
-let walls: Wall[] = generateWalls(4);
-const blades: Blade[] = generateBlades(random(4));
 
 const initX = CANVAS_WIDTH/3;
 const initY = CANVAS_HEIGHT - 2*WALL_HEIGHT_SMALL;
-
 const startWall = new Wall(initX,initY,"normal", "./images/normal-wall.png");
 const player =  new Player(initX+startWall.w,initY+WALL_HEIGHT_SMALL/2,"./images/grab-left.png");
 
-walls  = [startWall, ...walls];
+const gameState = {
+	blades: generateBlades(4),
+	walls: generateWalls(4),
+	layer: new Player(initX+startWall.w,initY+WALL_HEIGHT_SMALL/2,"./images/grab-left.png"),
+	airJump: false,
+}
+
+// let walls: Wall[] = generateWalls(4);
+// const blades: Blade[] = generateBlades(random(4));
+
+
+
+gameState.walls  = [startWall, ...gameState.walls];
 
 function Game() {
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 	player.draw();
-	walls.forEach((wall) => wall.draw());
-	blades.forEach((blade) => {
+	if(player.isJumping) player.jump();
+	gameState.walls.forEach((wall) => wall.draw());
+	gameState.blades.forEach((blade) => {
 		blade.draw();
 		blade.oscillate()
 	});
@@ -42,8 +49,17 @@ function Game() {
 
 gameWindow.addEventListener("click", () => {
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-	updateWalls(walls);
-	updateBlades(blades);
+	updateWalls(gameState.walls);
+	updateBlades(gameState.blades);	
+
+	if(player.jumpCount > 0){
+		player.isJumping = true;
+		player.resetJump();
+		--player.jumpCount;
+		console.log(player.dx,player.dy, player.xDirection, player.jumpCount);
+	}else{
+		player.isJumping = false;	
+	}
 });
 
 Game();
